0312c04c7bbb9835789ca61dc098335e8f53541e
LeGrumph <57810002+LeGrumph@users.noreply.github.com>
2020-07-13 18:15:15 +0200

    Partie 3 - Validation du prix de vente

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Min;
 
@@ -13,13 +14,13 @@ import javax.validation.constraints.Min;
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Length(min=3, max=20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
 
-    @Min(value = 1)
+    @Min(value = -1)
     private int prix;
 
     //information que nous ne souhaitons pas exposer
